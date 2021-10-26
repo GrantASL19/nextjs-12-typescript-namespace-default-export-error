89363bc0c029446100eeaed48bcde094e8caf071
Grant Heaslip <grant@asl19.org>
2021-10-26 17:51:16 -0400

    Add basic reproduction of namespace issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import appValues from '../values/appValues'
 
 const Home: NextPage = () => {
   return (
@@ -14,7 +15,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          {appValues.fullName}
         </h1>
 
         <p className={styles.description}>
