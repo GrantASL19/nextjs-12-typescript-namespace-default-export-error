19091afbc4fd2665b03bf3bb5da12e0fb53cd48d
Grant Heaslip <grant@asl19.org>
2021-10-26 20:43:16 -0400

    Make broken case the default (mixed up while testing)

--- values/appValues.ts
+++ values/appValues.ts
@@ -1,21 +1,21 @@
 // This doesn’t work, but does pass type checking since it’s valid TypeScript
 
-// namespace appValues {
-//   const firstName = "Foo";
-//   const lastName = "Bar";
-//   export const fullName = `${firstName} ${lastName}`
-// }
-//
-// export default appValues;
-
-// This works despite being roughly equivalent to the above namespace code.
-
-const firstName = "Foo";
-
-const lastName = "Bar";
-
-const appValues = {
-  fullName: `${firstName} ${lastName}`
+namespace appValues {
+  const firstName = "Foo";
+  const lastName = "Bar";
+  export const fullName = `${firstName} ${lastName}`
 }
 
 export default appValues;
+
+// This works despite being roughly equivalent to the above namespace code.
+
+// const firstName = "Foo";
+
+// const lastName = "Bar";
+
+// const appValues = {
+//   fullName: `${firstName} ${lastName}`
+// }
+
+// export default appValues;
